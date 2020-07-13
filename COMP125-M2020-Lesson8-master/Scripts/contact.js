684fde699a84ab16d65dd49a9dcd3bd103cb3d61
psychoCS <thiagoluizb@hotmail.com>
2020-07-13 16:16:25 -0400

    Added contact class

--- COMP125-M2020-Lesson8-master/Scripts/contact.js
+++ COMP125-M2020-Lesson8-master/Scripts/contact.js
@@ -10,6 +10,17 @@ let objects;
             this.contactNumber = contactNumber;
             this.shortMessage = ShortMessage;
         }
+
+        toString() {
+
+            let outputstring = "";
+            outputstring += "First name            : " + this.firstName + "\n";
+            outputstring += "Last name             : " + this.lastName + "\n";
+            outputstring += "Email address         : " + this.emailAddress + "\n";
+            outputstring += "Contact Number        : " + this.contactNumber + "\n";
+            outputstring += "Short Message         : " + this.shortMessage + "\n";
+            return outputstring;
+        }
     }
     objects.Contact = Contact
 
